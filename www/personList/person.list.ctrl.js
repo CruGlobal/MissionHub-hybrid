447075e2f8e7f2dc11cb76852da049d21801394a
Harro <michael.harrison@cru.org>
2015-09-11 17:14:11 -0400

    added search bar

--- www/personList/person.list.ctrl.js
+++ www/personList/person.list.ctrl.js
@@ -14,7 +14,7 @@ angular.module('missionhub')
   };
 
   that.refresh = function(config) {
-    config = config ? config : that.filters;
+    config = config ? config : that.filters();
     return api.people.get(config).then(function(data) {
       that.people = that.people.concat(data.people);
       that.hasMorePages = data.people.length < that.limit;
@@ -24,16 +24,28 @@ angular.module('missionhub')
   that.firstPage = function() {
     that.offset = 0;
     that.people = [];
-    that.refresh(that.filters).then(function(){
+    that.refresh(that.filters).then(function(data) {
+      $scope.$broadcast('scroll.refreshComplete');
+    }, function(error) {
       $scope.$broadcast('scroll.refreshComplete');
     });
   };
 
   that.nextPage = function() {
     that.offset += that.limit;
-    that.refresh(that.filters).then(function(){
+    that.refresh(that.filters).then(function() {
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    }, function(error) {
       $scope.$broadcast('scroll.infiniteScrollComplete');
     });
   };
+
+//  $scope.watch('searchTerm', function(newValue, oldValue) {
+//    if (newValue !== '') {
+//      var params = that.filters();
+//      params['name_or_email_like'] = newValue;
+//      that.refresh(params);
+//    }
+//  });
 
 });
