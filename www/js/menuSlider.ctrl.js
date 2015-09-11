0c6bd6128de96618e2a9300aa80943d540a90bb3
Spencer Oberstadt <soberstadt@gmail.com>
2015-09-11 10:10:07 -0400

    a few little things for login

--- www/js/menuSlider.ctrl.js
+++ www/js/menuSlider.ctrl.js
@@ -1,54 +1,76 @@
 angular.module('missionhub')
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  var that = this;
-  this.loginData = {};
+    var that = this;
+    this.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('personList/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.loginModal = modal;
-  });
-  $ionicModal.fromTemplateUrl('personList/filters.html', {
-    scope: $scope
-  }).then(function(modal) {
-    that.filtersModal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('personList/login.html', {
+      scope: $scope,
+      backdropClickToClose: false,
+      hardwareBackButtonClose: false
+    }).then(function(modal) {
+      $scope.loginModal = modal;
+      //$scope.loginModal.show();
+    });
+    $ionicModal.fromTemplateUrl('personList/filters.html', {
+      scope: $scope
+    }).then(function(modal) {
+      that.filtersModal = modal;
+    });
 
-  $scope.closeLogin = function() {
-    $scope.loginModal.hide();
-  };
+    $scope.closeLogin = function() {
+      $scope.loginModal.hide();
+    };
 
-  $scope.login = function() {
-    $scope.loginModal.show();
-  };
+    $scope.login = function() {
+      $scope.loginModal.show();
+    };
 
-  this.openFilters = function() {
-    this.filtersModal.show();
-  }
-  $scope.closeFilters = function() {
-    that.filtersModal.hide();
-  };
+    this.openFilters = function() {
+      this.filtersModal.show();
+    }
+    $scope.closeFilters = function() {
+      that.filtersModal.hide();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', that.loginData);
+    if(window.facebookConnectPlugin && facebookConnectPlugin.getLoginStatus) {
+      window.facebookConnectPlugin.getLoginStatus(function(data){
+        console.log(data)
+      })
+    }
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
+    $scope.facebookLogin = function() {
+      if(window.facebookConnectPlugin) {
+        facebookConnectPlugin.login(["public_profile"],
+          function(userData) {
+            console.log(userData);
+            localStorage.setItem('fb-login-info', JSON.stringify(userData))
+          },
+          function(message) {
+            alert('Could not login: '+message)
+          });
+      }
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+      console.log('Doing login', that.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function() {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
 
 
