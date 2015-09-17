4aad9bf112a5da0ec1ee3313887fc0a037083baf
Harro <michael.harrison@cru.org>
2015-09-17 14:43:22 -0400

    added labels to profile

--- www/profile/person.show.ctrl.js
+++ www/profile/person.show.ctrl.js
@@ -2,9 +2,10 @@ angular.module('missionhub')
 .controller('PersonShowCtrl', function($scope, $stateParams, api) {
     var that = this;
 
+    that.includes = ['organizational_permission' ,'permission', 'organizational_labels', 'label', 'interactions', 'email_addresses', 'phone_numbers', 'addresses'];
     that.person = {};
 
-    api.people.get({id: $stateParams.contactId, include: 'interactions'}).then(function(data) {
+    api.people.get({id: $stateParams.contactId, include: that.includes.join()}).then(function(data) {
       that.person = data.person;
     }, function(error) {
 
