7bf4ae863fd583e569caf9083e595fb86727123d
Harro <michael.harrison@cru.org>
2015-09-17 13:41:50 -0400

    added size to personAvatar filter

--- www/filters/person.filters.js
+++ www/filters/person.filters.js
@@ -1,14 +1,17 @@
 angular.module('missionhub')
 .filter('personAvatar', function() {
 
-  return function (person) {
-    if (!person) {
+  return function (person, size) {
+
+    size = size || 40;
+
+    if (!person || !person.first_name) {
       return '';
     }
     if (person.picture) {
-      return person.picture;
+      return person.picture + '?width=' + size + '&height=' + size;
     }
     return "https://cdn.discourse.org/ionicframework/letter_avatar/" + person.first_name +
-      "/40/5_fcf819f9b3791cb8c87edf29c8984f83.png";
+      "/" + size + "/5_fcf819f9b3791cb8c87edf29c8984f83.png";
   };
 });
