511faae2ee74f1ff698cc3698cf3b01a0ceba8bb
Harro <michael.harrison@cru.org>
2015-09-17 16:21:37 -0400

    tabs now switch out content

--- www/profile/person.show.ctrl.js
+++ www/profile/person.show.ctrl.js
@@ -4,11 +4,21 @@ angular.module('missionhub')
 
     that.includes = ['organizational_permission' ,'permission', 'organizational_labels', 'label', 'interactions', 'email_addresses', 'phone_numbers', 'addresses'];
     that.person = {};
+    that.currentTab = 0;
 
     api.people.get({id: $stateParams.contactId, include: that.includes.join()}).then(function(data) {
       that.person = data.person;
     }, function(error) {
 
     });
+
+    that.setTab = function(tab) {
+      tab = tab || 0;
+      that.currentTab = tab;
+    };
+
+    that.isCurrentTab = function(tab) {
+      return that.currentTab === tab;
+    };
 
 });
