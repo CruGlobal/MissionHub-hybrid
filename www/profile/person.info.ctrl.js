47506412a53872058c9d7b15f9df081e1077115c
Spencer Oberstadt <soberstadt@gmail.com>
2015-09-22 14:55:11 -0400

    filter out blank addresses

--- www/profile/person.info.ctrl.js
+++ www/profile/person.info.ctrl.js
@@ -1,7 +1,18 @@
 angular.module('missionhub')
-  .controller('PersonInfoCtrl', function($scope) {
+  .controller('PersonInfoCtrl', function($scope, $filter) {
     var that = this;
 
     that.person = $scope.person || {};
+    function filterEmptyAddresses() {
+      return $filter('filter')(that.person.addresses, function(address) {
+        return !!address.address1;
+      })
+    }
+    that.person.addresses = filterEmptyAddresses();
 
+    $scope.$watch('person.addresses', function() {
+      var cleanArray = filterEmptyAddresses();
+      if(cleanArray && cleanArray.length != that.person.addresses.length)
+        that.person.addresses = filterEmptyAddresses();
+    });
   });
